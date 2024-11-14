92d0e57ecd22c0be72d22dc21db795bcb86ffc11
zheng5862 <108252962+zheng5862@users.noreply.github.com>
2024-11-14 09:57:11 +0800

    Confounder.R

---
+++ Confounder.R
@@ -0,0 +1,13 @@
+easy_confouder(single_file="Table1.csv",
+               mult_file=F,
+
+               query=T,
+               bind_result=T,
+               split_file=T,
+               ensemble=T,
+               SNP="rsid",
+               r2=0.8,
+               GRCh=37,
+               num1=1,
+               num2=50,
+               select_confounders=c("BMI"))
